bc9a65ef7b8301c5ded6e1b52015171b7c7b1a53
ittiponttc <ittipon.kmutnb@gmail.com>
2026-01-12 16:00:27 +0700

    Update Flexible-Pavement-AASHTO.py

--- Flexible-Pavement-AASHTO.py
+++ Flexible-Pavement-AASHTO.py
@@ -3,6 +3,7 @@ import numpy as np
 import math
 import pandas as pd
 
+# ===== ADD HERE =====
 MATERIAL_LIBRARY = {
     "Surface Course": {
         "Asphalt Concrete (Dense-graded)": {"a": 0.42, "m": 1.00},
@@ -17,6 +18,8 @@ MATERIAL_LIBRARY = {
         "Granular Subbase": {"a": 0.11, "m": 1.00},
         "Sand / Sandy Gravel": {"a": 0.08, "m": 1.00},
     }
+}
+# =====================
 
 
 def bisection_method(func, a, b, tol=1e-6, max_iter=100):
